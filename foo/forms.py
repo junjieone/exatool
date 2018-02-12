0d62ccf8ba9f35e1f908009e6b6a898a4dcdc0ba
junjie <junjie@juniper.net>
2018-02-12 19:15:56 +0800

    first upload

---
+++ foo/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class ParamForm(forms.Form):
+   is_nb_addr = forms.CharField(max_length = 50, label='IS neighbor Address')
+   is_pref = forms.CharField(max_length = 50, label='IS prefix')
+   is_es_nx_hop = forms.CharField(max_length = 50, label='ES next-hop of IS')
+   is_vnh_label = forms.CharField(max_length = 50, label='VNH label of IS')
+   dr_nb_addr = forms.CharField(max_length = 50, label='DR neighbor address')
+   dr_pref = forms.CharField(max_length=50, label='DR prefix')
+   dr_es_nx_hop = forms.CharField(max_length=50, label='ES next-hop of DR')
+   dr_vnh_label = forms.CharField(max_length=50, label='VNH label of DR')
+   dr_sr_addr = forms.CharField(max_length=50, label='DR source address')
+   dr_dt_addr = forms.CharField(max_length=50, label='DR destination address')
--- foo/views.py
+++ foo/views.py
@@ -3,7 +3,22 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from forms import *
+import jinja2
 
 # Create your views here.
 def execute(request):
-    return render(request, "execute.html", {})
+    if request.method == "POST":
+        form = ParamForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            with open('./foo/files/template.j2', 'r') as f:
+                template = jinja2.Template(f.read())
+                text = template.render(params=form.cleaned_data)
+                print(text)
+                f.close()
+        paramForm = ParamForm()
+        return render(request, "execute.html", {'paramForm': paramForm, 'response':text})
+    else:
+        paramForm = ParamForm()
+        return render(request, "execute.html", {'paramForm':paramForm})
