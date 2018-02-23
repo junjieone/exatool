ed5eb1a970fa59b793b32c11e773fb4c049098c3
junjie <junjie@juniper.net>
2018-02-24 01:59:43 +0800

    first upload

--- foo/forms.py
+++ foo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ParamForm(forms.Form):
    is_nb_addr = forms.CharField(max_length = 50, label='IS neighbor Address')
-   is_pref = forms.CharField(max_length = 50, label='IS prefix')
+   is_pref = forms.CharField(max_length = 200, label='IS prefix', widget=forms.TextInput(attrs={'size':100}))
    is_es_nx_hop = forms.CharField(max_length = 50, label='ES next-hop of IS')
    is_vnh_label = forms.CharField(max_length = 50, label='VNH label of IS')
    dr_nb_addr = forms.CharField(max_length = 50, label='DR neighbor address')
